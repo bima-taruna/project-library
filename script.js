814f666d520e29c5f13b318943091e325199f698
bima-taruna <bimataruna21@gmail.com>
2024-04-04 13:49:15 +0700

    create create card and load book function

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-function Book(title, author, pages, isRead) {
+const myBook = [
+  {
+    title: "The Hobbit",
+    author: "J.R.R Tolkien",
+    pages: 295,
+    isRead: false,
+  },
+];
+const readButton = document.createElement("button");
+const deleteButton = document.createElement("button");
+readButton.classList.add("read-button");
+readButton.textContent = "Read";
+const books = document.querySelector(".books");
+
+function Book(...params) {
+  const [title, author, pages, isRead] = params;
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.isRead = isRead;
+  this.toogleRead = function () {
+    this.isRead = !this.isRead;
+  };
   this.info = function () {
     return `${this.title} by ${this.author}, ${this.pages} pages, ${
       isRead ? "already read" : "not read yet"
@@ -10,6 +28,55 @@ function Book(title, author, pages, isRead) {
   };
 }
 
-const book1 = new Book("The Hobbit", "J.R.R Tolkien", 295, false);
+function addBookToLibrary(...params) {
+  const newBook = new Book(...params);
+  myBook.push(newBook);
+}
 
-console.log(book1.info());
+function createCard(...params) {
+  const [title, author, pages, isRead] = params;
+  const card = document.createElement("div");
+  const content = document.createElement("ul");
+  const titleList = document.createElement("li");
+  const authorList = document.createElement("li");
+  const pagesList = document.createElement("li");
+  const readList = document.createElement("li");
+  titleList.classList.add("title");
+  authorList.classList.add("author");
+  pagesList.classList.add("pages");
+  readList.classList.add("read");
+  titleList.textContent = title;
+  authorList.textContent = author;
+  pagesList.textContent = pages + " pages";
+  readButton.textContent = isRead ? "already read" : "not read yet";
+  readList.appendChild(readButton);
+  content.classList.add("content");
+  card.classList.add("card");
+  content.appendChild(titleList);
+  content.appendChild(authorList);
+  content.appendChild(pagesList);
+  content.appendChild(readList);
+  card.appendChild(createDeleteButton());
+  card.appendChild(content);
+  return card;
+}
+
+function createDeleteButton() {
+  const trashImage = document.createElement("img");
+  trashImage.classList.add("icon");
+  trashImage.setAttribute("src", "icons/trash-can.png");
+  trashImage.setAttribute("alt", "delete button");
+  deleteButton.classList.add("delete-button");
+  deleteButton.appendChild(trashImage);
+  return deleteButton;
+}
+
+function loadBooks() {
+  myBook.forEach((item) => {
+    books.appendChild(
+      createCard(item.title, item.author, item.pages, item.title)
+    );
+  });
+}
+
+loadBooks();
