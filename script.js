3d6f528e596eb244689257b165078b4fc5fb05aa
bima-taruna <bimataruna21@gmail.com>
2024-04-18 21:35:07 +0700

    create card with template html instead

--- script.js
+++ script.js
@@ -1,145 +1,126 @@
-const myBook = [];
+let library = (function () {
+  const myBook = [];
+  let modalTrigger = false;
+  const addButton = document.querySelector(".add-button");
+  const addBookForm = document.querySelector(".add-book");
+  const overlay = document.querySelector(".overlay");
+  const title = document.getElementById("title");
+  const author = document.getElementById("author");
+  const pages = document.getElementById("pages");
+  const isRead = document.getElementById("isRead");
+  const books = document.querySelector(".books");
+  const closeModalButton = document.querySelector(".close-modal");
 
-const addButton = document.querySelector(".add-button");
-const addBookForm = document.querySelector(".add-book");
-const overlay = document.querySelector(".overlay");
-const modal = document.querySelector(".modal");
-const title = document.getElementById("title");
-const author = document.getElementById("author");
-const pages = document.getElementById("pages");
-const isRead = document.getElementById("isRead");
-const books = document.querySelector(".books");
-const closeModalButton = document.querySelector(".close-modal");
-let modalTrigger = false;
+  addButton.addEventListener("click", () => {
+    modalTrigger = !modalTrigger;
+    toogleModal();
+  });
 
-addBookForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  addBookToLibrary(title.value, author.value, pages.value, isRead.checked);
-  modalTrigger = false;
-  toogleModal();
+  addBookForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    addBookToLibrary(title.value, author.value, pages.value, isRead.checked);
+  });
+
+  closeModalButton.addEventListener("click", () => {
+    modalTrigger = false;
+    toogleModal();
+  });
+
   loadBooks();
-  giveReadFunction();
-  giveDeleteFunction();
-});
 
-closeModalButton.addEventListener("click", () => {
-  modalTrigger = false;
-  toogleModal();
-});
+  function loadBooks() {
+    while (books.children.length > 0) {
+      books.removeChild(books.firstChild);
+    }
 
-function Book(...params) {
-  const [title, author, pages, isRead] = params;
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.isRead = isRead;
-  this.info = function () {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${
-      isRead ? "already read" : "Haven't read yet"
-    }`;
+    myBook.forEach((item) => {
+      const newCard = document.createElement("div");
+      newCard.classList.add("card");
+      let content = `
+      <button class="delete-button">
+        <img class="icon" src="icons/trash-can.png" alt="delete button">
+      </button>
+      <ul class="content">
+        <li class="title">${item.title}</li>
+        <li class="author">${item.author}</li>
+        <li class="pages">${item.pages} pages</li>
+        <li class="read">
+          <button class="read-button" style="background-color: ${
+            item.isRead ? "#abc4aa" : "red"
+          }; color: ${isRead ? "#675d50" : "white"};">${
+        item.isRead ? "Already read" : "Not read yet"
+      }</button>
+        </li>
+      </ul>
+    `;
+      newCard.innerHTML = content;
+      books.appendChild(newCard);
+    });
+    console.log(myBook);
+  }
+
+  function Book(...params) {
+    const [title, author, pages, isRead] = params;
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+    this.info = function () {
+      return `${this.title} by ${this.author}, ${this.pages} pages, ${
+        isRead ? "already read" : "Haven't read yet"
+      }`;
+    };
+  }
+
+  Book.prototype.toogleRead = function () {
+    this.isRead = !this.isRead;
   };
-}
 
-Book.prototype.toogleRead = function () {
-  this.isRead = !this.isRead;
-};
-
-function addBookToLibrary(...params) {
-  const newBook = new Book(...params);
-  myBook.push(newBook);
-}
-
-function createCard(...params) {
-  const [title, author, pages, isRead] = params;
-  const readButton = document.createElement("button");
-  const card = document.createElement("div");
-  const content = document.createElement("ul");
-  const titleList = document.createElement("li");
-  const authorList = document.createElement("li");
-  const pagesList = document.createElement("li");
-  const readList = document.createElement("li");
-  titleList.classList.add("title");
-  authorList.classList.add("author");
-  pagesList.classList.add("pages");
-  readList.classList.add("read");
-  readButton.classList.add("read-button");
-  readButton.textContent = "Read";
-  titleList.textContent = title;
-  authorList.textContent = author;
-  pagesList.textContent = pages + " pages";
-  readButton.textContent = isRead ? "Already Read" : "Not read yet";
-  readButton.style.backgroundColor = isRead ? "#abc4aa" : "red";
-  readButton.style.color = isRead ? "#675d50" : "white";
-  readList.appendChild(readButton);
-  content.classList.add("content");
-  card.classList.add("card");
-  content.appendChild(titleList);
-  content.appendChild(authorList);
-  content.appendChild(pagesList);
-  content.appendChild(readList);
-  card.appendChild(createDeleteButton());
-  card.appendChild(content);
-  return card;
-}
-
-function createDeleteButton() {
-  const trashImage = document.createElement("img");
-  const deleteButton = document.createElement("button");
-  trashImage.classList.add("icon");
-  trashImage.setAttribute("src", "icons/trash-can.png");
-  trashImage.setAttribute("alt", "delete button");
-  deleteButton.classList.add("delete-button");
-  deleteButton.appendChild(trashImage);
-  return deleteButton;
-}
-
-function loadBooks() {
-  while (books.children.length > 0) {
-    books.removeChild(books.firstChild);
+  function addBookToLibrary(...params) {
+    const newBook = new Book(...params);
+    myBook.push(newBook);
+    modalTrigger = false;
+    toogleModal();
+    loadBooks();
+    giveReadFunction();
+    giveDeleteFunction();
   }
-  myBook.forEach((item) => {
-    books.appendChild(
-      createCard(item.title, item.author, item.pages, item.isRead)
-    );
-  });
-  console.log(myBook);
-}
 
-function toogleModal() {
-  if (modalTrigger) {
-    overlay.style.display = "block";
-  } else {
-    overlay.style.display = "none";
+  function toogleModal() {
+    if (modalTrigger) {
+      overlay.style.display = "block";
+    } else {
+      overlay.style.display = "none";
+    }
   }
-}
 
-function giveReadFunction() {
-  const readButtons = document.querySelectorAll(".read-button");
-  readButtons.forEach((button, i) => {
-    button.addEventListener("click", () => {
-      myBook[i].toogleRead();
-      loadBooks();
-      giveReadFunction();
-      giveDeleteFunction();
+  function giveReadFunction() {
+    const readButtons = document.querySelectorAll(".read-button");
+    readButtons.forEach((button, i) => {
+      button.addEventListener("click", () => {
+        myBook[i].toogleRead();
+        loadBooks();
+        giveReadFunction();
+        giveDeleteFunction();
+      });
     });
-  });
-}
+  }
 
-function giveDeleteFunction() {
-  const deleteButtons = document.querySelectorAll(".delete-button");
-  deleteButtons.forEach((button, i) => {
-    button.addEventListener("click", () => {
-      myBook.splice(i, 1);
-      loadBooks();
-      giveReadFunction();
-      giveDeleteFunction();
+  function giveDeleteFunction() {
+    const deleteButtons = document.querySelectorAll(".delete-button");
+    deleteButtons.forEach((button, i) => {
+      button.addEventListener("click", () => {
+        myBook.splice(i, 1);
+        loadBooks();
+        giveReadFunction();
+        giveDeleteFunction();
+      });
     });
-  });
-}
+  }
 
-addButton.addEventListener("click", () => {
-  modalTrigger = !modalTrigger;
-  toogleModal();
-});
+  function deleteBooks(params) {}
 
-loadBooks();
+  return {
+    addBookToLibrary,
+  };
+})();
---
+++ script2.js
@@ -0,0 +1,16 @@
+var library = (function () {
+  let books = [];
+  let modalTrigger = false;
+  const addButton = document.querySelector(".add-button");
+  const addBookForm = document.querySelector(".add-book");
+  const overlay = document.querySelector(".overlay");
+  const modal = document.querySelector(".modal");
+  const title = document.getElementById("title");
+  const author = document.getElementById("author");
+  const pages = document.getElementById("pages");
+  const isRead = document.getElementById("isRead");
+  const booksDom = document.querySelector(".books");
+  const closeModalButton = document.querySelector(".close-modal");
+
+  addBookForm.addEventListener("submit");
+})();
