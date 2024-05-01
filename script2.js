f3df01a02f98f4907a5a8ab4cfee6b4936d26a8a
bima-taruna <bimataruna21@gmail.com>
2024-05-01 16:13:15 +0700

    bind event listener to add button and create toogle modal method

--- script2.js
+++ script2.js
@@ -17,3 +17,32 @@ class Book {
     this.isRead = !this.isRead;
   }
 }
+
+class Library {
+  #myBook = [];
+  #modalTrigger = false;
+  #addButton = document.querySelector(".add-button");
+  #addBookForm = document.querySelector(".add-book");
+  #overlay = document.querySelector(".overlay");
+  #title = document.getElementById("title");
+  #author = document.getElementById("author");
+  #pages = document.getElementById("pages");
+  #isRead = document.getElementById("isRead");
+  #books = document.querySelector(".books");
+  #closeModalButton = document.querySelector(".close-modal");
+
+  constructor() {
+    this.#addButton.addEventListener("click", () => {
+      this.#modalTrigger = !this.#modalTrigger;
+      this.toogleModal();
+    });
+  }
+
+  toogleModal() {
+    if (modalTrigger) {
+      overlay.style.display = "block";
+    } else {
+      overlay.style.display = "none";
+    }
+  }
+}
