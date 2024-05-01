4c22cad4f043368c282d43af35871a0781cadc4b
bima-taruna <bimataruna21@gmail.com>
2024-05-01 16:26:28 +0700

    bind event listener to close modal and add book form, also create loadbooks and addBookToLibrary method

--- script2.js
+++ script2.js
@@ -32,17 +32,72 @@ class Library {
   #closeModalButton = document.querySelector(".close-modal");
 
   constructor() {
+    this.loadBooks;
+
     this.#addButton.addEventListener("click", () => {
       this.#modalTrigger = !this.#modalTrigger;
-      this.toogleModal();
+      this.toogleModal;
+    });
+
+    this.#addBookForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+      this.addBookToLibrary(
+        title.value,
+        author.value,
+        pages.value,
+        isRead.checked
+      );
+    });
+
+    this.#closeModalButton.addEventListener("click", () => {
+      this.#modalTrigger = false;
+      this.toogleModal;
     });
   }
 
-  toogleModal() {
-    if (modalTrigger) {
+  loadBooks = () => {
+    while (this.#books.children.length > 0) {
+      this.#books.removeChild(this.#books.firstChild);
+    }
+
+    this.#myBook.forEach((item) => {
+      const newCard = document.createElement("div");
+      newCard.classList.add("card");
+      let content = `
+      <button class="delete-button">
+        <img class="icon" src="icons/trash-can.png" alt="delete button">
+      </button>
+      <ul class="content">
+        <li class="title">${item.title}</li>
+        <li class="author">${item.author}</li>
+        <li class="pages">${item.pages} pages</li>
+        <li class="read">
+          <button class="read-button" style="background-color: ${
+            item.isRead ? "#abc4aa" : "red"
+          }; color: ${item.isRead ? "#675d50" : "white"};">${
+        item.isRead ? "Already read" : "Not read yet"
+      }</button>
+        </li>
+      </ul>
+    `;
+      newCard.innerHTML = content;
+      this.#books.appendChild(newCard);
+    });
+  };
+
+  addBookToLibrary = (...params) => {
+    const newBook = new Book(...params);
+    this.#myBook.push(newBook);
+    this.#modalTrigger = false;
+    this.toogleModal;
+    this.loadBooks;
+  };
+
+  toogleModal = () => {
+    if (this.#modalTrigger) {
       overlay.style.display = "block";
     } else {
       overlay.style.display = "none";
     }
-  }
+  };
 }
